68a5533fae60b59f9cae4b8a79a48204024a5b2c
cwang <12345@qq.com>
2018-05-14 10:41:35 +0800

    city router

---
+++ src/pages/city/components/CityHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+    
+  </div>
+  
+</template>
+
+<script>
+  
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+  .header
+    position: relative
+    overflow: hidden
+    height : $header-height
+    line-height : $header-height
+    text-align :center
+    color : #fff
+    background :$bgColor
+    font-size : .35rem
+    .header-back
+      position: absolute
+      top: 0
+      left :0
+      width :.64rem
+      font-size :.4rem
+      text-align : center 
+      color :#fff
+</style>
