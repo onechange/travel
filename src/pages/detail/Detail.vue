1626d238a99c733dfba11ee1dbd3ff2e2fa9d2a1
cwang <12345@qq.com>
2018-05-17 23:27:08 +0800

    add detailList

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,20 +1,47 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
+import DetailList from './components/List'
+
 export default {
   name: 'Detail',
   components: {
-    DetailBanner 
+    DetailBanner,
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list:[{
+        title: '成人票',
+        children: [{
+          title: '三联票'}, {
+            title: '五联票'
+          }]
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .content
+    height 50rem
 </style>
 
